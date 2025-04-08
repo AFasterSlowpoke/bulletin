873689031ddb256d95e10ab5f0b12d44708d358c
Caleb Ruszkowski <caleb.ruszkowski@gmail.com>
2025-04-08 19:07:04 -0400

    Created paint() method

--- bulletin.py
+++ bulletin.py
@@ -17,10 +17,29 @@ class Board:
         
         return f"Board\n{len(self.pins)} Pins\n__________\n{pins_string}"
     
-    def post(self, index):
+    def paint(self, pin, data_index):
+        """
+        Adds a single pin to the image of a single row.
+        """
+
+        if type(pin) is not Pin:
+            raise TypeError("Board object tried to paint a non-Pin object")
+        
+        data_row = self.data.iloc[data_index] # Get the row at index
+        content = data_row[pin.col] # Get the data at given column specified in pin
+
+        print(content)
+    
+    def post(self, data_index):
+        """
+        Creates the appropriate image for a single row of the data.
+        """
         pass
 
     def publish(self):
+        """
+        Creates all the images for the data.
+        """
         pass
 
     def pin(self, *args):
@@ -31,11 +50,12 @@ class Board:
                 raise TypeError("Board.pin() method only accepts Pin objects")
 
 class Pin:
-    def __init__(self, title):
+    def __init__(self, title, col):
         self.title = title
+        self.col = col
     
     def __str__(self):
-        return f"Pin: {self.title}"
+        return f"Pin: {self.title}, Column: {self.col}"
 
 class TextPin(Pin):
     pass
