7cb054f622ca1cd7174207b41805c94e367da20e
Caleb Ruszkowski <caleb.ruszkowski@gmail.com>
2025-04-10 16:37:09 -0400

    Added _text_paint() method and properties for TextPin object

--- bulletin.py
+++ bulletin.py
@@ -16,19 +16,28 @@ class Board:
             pins_string += f"{str(pin)}\n"
         
         return f"Board\n{len(self.pins)} Pins\n__________\n{pins_string}"
+
+    def _text_paint(self, canvas, pin, content):
+        canvas.show()
     
-    def paint(self, pin, data_index):
+    def paint(self, canvas, pin, data_index):
         """
         Adds a single pin to the image of a single row.
         """
 
-        if type(pin) is not Pin:
+        if not isinstance(pin, Pin):
             raise TypeError("Board object tried to paint a non-Pin object")
         
         data_row = self.data.iloc[data_index] # Get the row at index
         content = data_row[pin.col] # Get the data at given column specified in pin
 
-        print(content)
+        # Pass the pin, canvas and content to the appropriate paint function
+        if isinstance(pin, TextPin):
+            self._text_paint(canvas, pin, content)
+        elif isinstance(pin, ImagePin):
+            print(f"{pin} is an ImagePin")
+        else:
+            print(f"{pin} is neither a TextPin or ImagePin")
     
     def post(self, data_index):
         """
@@ -43,22 +52,36 @@ class Board:
         pass
 
     def pin(self, *args):
+        """
+        Adds a list of pins to the board.
+        """
+
         for arg in args:
-            if type(arg) is Pin:
+            if isinstance(arg, Pin):
                 self.pins.append(arg)
             else:
                 raise TypeError("Board.pin() method only accepts Pin objects")
 
 class Pin:
-    def __init__(self, title, col):
+    def __init__(self, title, col, pos):
         self.title = title
         self.col = col
+        self.pos = pos
     
     def __str__(self):
-        return f"Pin: {self.title}, Column: {self.col}"
+        return f"Pin: {self.title}, Column: {self.col}, Position: {self.pos}"
 
 class TextPin(Pin):
-    pass
+    def __init__(self, title, col, pos, font, font_size, max_length, fill_mode="shrink"):
+        super().__init__(title, col, pos)
+
+        self.font = font
+        self.font_size = font_size
+        self.max_length = max_length
+        self.fill_mode = fill_mode
+
+    def __str__(self):
+        return f"TextPin: {self.title}, Column: {self.col}, Position: {self.pos}, Font: {self.font}"
 
 class ImagePin(Pin):
     pass
